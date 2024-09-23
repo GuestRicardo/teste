00418fd6bbb94e4dc0261211a1ed857cd1fa8b90
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-23 09:54:59 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: const Text('Aplicativo exemplo scaffold'),
       ),
-      body: const Text('Meu aplicativo'),
+      body: const Center(child: Text('Meu aplicativo')),
     ));
   }
 }
