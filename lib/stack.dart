c762639afb5237fb4eded500b91b82851aa0b5bc
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-23 18:11:59 -0300

    formatando

---
+++ lib/stack.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Aplicativo stack'),
+      ),
+      body: Stack(
+        fit: StackFit.loose,
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue[900],
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: const Color.fromARGB(255, 161, 13, 13),
+          ),
+        ],
+      ),
+    ));
+  }
+}
