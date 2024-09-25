29edd587b3e8a6cd4e6a21d7f0ceaac67a1bbd4d
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-25 17:29:25 -0300

    criando rotas

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,52 @@ class Tela1 extends StatelessWidget {
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Tela 1'),
+            title: const Text('Tela 1'),
+            backgroundColor: Colors.blue,
+          ),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return const Tela2();
+                  }),
+                );
+              },
+              child: const Text('Ir para proxima tela.'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tela 2'),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return const Tela1();
+                  }),
+                );
+              },
+              child: const Text('Voltar uma tela.'),
+            ),
           ),
         ),
       ),
