3a9ba27561aafe8b7bda50f8b8c0f46817117c38
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-30 08:56:55 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  /// Observação tudo que estiver com apropriedade e : sera opcional
+  /// exemplo:
+  /// fontStyle : FontStyle.italic,
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,13 +17,16 @@ class MyApp extends StatelessWidget {
       //se caso esteja dentro, nao precisa do TextDirection
       child: const Center(
         child: Text(
-          'teste',
+          'teste\nTexto 2', //colocando o texto em 2 linha usase o \n
           textDirection: TextDirection.ltr,
           style: TextStyle(
             fontSize: 50, // tamanho da fonte
+            height: 1.15, //espaçamento de linhas
             fontWeight: FontWeight.w200, //fonte em negrito, ou so escreve bold, ou normal
-            color: Color.blue.shade700, //cores e tonalidades
-
+            color: Color.fromARGB(255, 199, 179, 0), //cores e tonalidades
+            backgroundColor: Colors.red, //aq esta trocando o fundo somente do texto
+            fontStyle: FontStyle.italic, //colocando em italico
+            decoration: TextDecoration.overline, //linhas no texto podendo ser no meio embaixo, e embaixo so testar as opções
           ),
         ),
       ),
