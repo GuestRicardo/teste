a5406c9eaf797f33ed64565d21a9476fcfab4679
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-30 08:22:39 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -4,86 +4,13 @@ void main() {
   runApp(const MyApp());
 }
 
-//os parametros que seram passados
-class Argumentos {
-  final int id;
-  final String nome;
-
-  Argumentos(this.id, this.nome);
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Tela1(),
-        Tela2.routeName: (context) => const Tela2(),
-      },
-    );
-  }
-}
-
-class Tela1 extends StatelessWidget {
-  const Tela1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Tela 1'),
-            backgroundColor: Colors.blue,
-          ),
-          body: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/tela2',
-                    arguments: Argumentos(1, 'Maria'));
-              },
-              child: const Text('Ir para proxima tela.'),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Tela2 extends StatelessWidget {
-  //identificação da rota dentro do widget
-  static const routeName = '/tela2';
-
-  const Tela2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-//para receber os argumentos
-    final argumentos = ModalRoute.of(context)?.settings.arguments as Argumentos;
-
-    return Container(
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            //recebendo os argumentos
-            title: const Text('Tela 2 ${argumentos.nome}'),
-            backgroundColor: Colors.red,
-          ),
-          body: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Voltar uma tela.'),
-            ),
-          ),
-          backgroundColor: Colors.black,
-        ),
-      ),
+      child: const Text('teste'),
     );
   }
 }
