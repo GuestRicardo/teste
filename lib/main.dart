af299a2372d6f05220ecfc96df708895069002c0
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-30 14:36:52 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,16 @@ class Home extends StatelessWidget {
         appBar: AppBar(title: const Center(child: Text('Quiz - Estudos'))),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               const FlutterLogo(size: 300),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text('Iniciar', style: ,),
+                onPressed: () {
+                  print('teste');
+                },
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+                child: const Text('Iniciar', style: TextStyle(fontSize: 50)),
               ),
             ],
           ),
