cde6e0f1d1cc5ff2941fd1ea4e7effc2c0c92f31
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-09-30 08:43:35 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,18 @@ class MyApp extends StatelessWidget {
     return Container(
       //essas propriedades se tornam obrigatorias quando estao fora do Scaffold,
       //se caso esteja dentro, nao precisa do TextDirection
-      child: const Text('teste', textDirection: TextDirection.ltr),
+      child: const Center(
+        child: Text(
+          'teste',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 50, // tamanho da fonte
+            fontWeight: FontWeight.w200, //fonte em negrito, ou so escreve bold, ou normal
+            color: Color.blue.shade700, //cores e tonalidades
+
+          ),
+        ),
+      ),
     );
   }
 }
