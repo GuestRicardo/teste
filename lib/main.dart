a4a87583445539221afd8ebe1c2bfb6c69036b23
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-10-01 14:45:09 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,6 @@
 import 'package:flutter/material.dart';
+import 'homePage.dart';
 
 void main() {
   runApp(const Home());
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark, //modoNoturno ou .light q é o modo claro
-        primaryColor: const Color.fromARGB(
-            255, 16, 92, 143), //barras, botoes, qualquer componente do app
-      ),
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 12, 181, 187),
-        appBar: AppBar(title: const Center(child: Text('Quiz - Estudos'))),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset('assets/images/logo.png'),
-              ElevatedButton(
-                onPressed: () {
-                  print('teste');
-                },
-                style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
-                child: const Text('Iniciar', style: TextStyle(fontSize: 50)),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
