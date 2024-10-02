025380b89aed53fcdfc0906ebab67351972a08f9
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-10-02 08:47:19 -0300

    formatando

---
+++ lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+
+void main() {
+  runApp(const Quiz());
+}
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark, //modoNoturno ou .light q é o modo claro
+        primaryColor: const Color.fromARGB(
+            255, 16, 92, 143), //barras, botoes, qualquer componente do app
+      ),
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 12, 181, 187),
+        appBar: AppBar(title: const Center(child: Text('Quiz - Estudos'))),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print('teste');
+                },
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+                child: const Text('Iniciar', style: TextStyle(fontSize: 50)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
