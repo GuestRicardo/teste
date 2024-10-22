84355e04c8e8bd5a313ff4cf23029aa6f31a9727
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-10-22 13:45:12 -0300

    formatando

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:teste/ESTUDOS/calculadora.dart';
 import 'homePage.dart';
 import 'quiz.dart';
 import 'resultado.dart';
 
 void main() {
-  runApp(Resultado(acertos: 3));
+  // runApp(Resultado(acertos: 3));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
