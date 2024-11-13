7421fae1661d5dd532c176bd757c05be8fbdea10
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-11-13 09:35:37 -0300

    formatando

--- lib/ESTUDOS/progAssicrona.dart
+++ lib/ESTUDOS/progAssicrona.dart
@@ -3,14 +3,18 @@ import 'dart:async';
 void main() {
 //Assicrona
 
-//aq esta sendo criado uma variavel tipo void q retornara somente um print, e tera uma espera de 3 segundos via o delay
-  Future<void> quatro = Future.delayed(Duration(seconds: 3));
-//aq ele vai imprimir o numero o 4
-  quatro.then((value) => print(4));
+// //aq esta sendo criado uma variavel tipo void q retornara somente um print, e tera uma espera de 3 segundos via o delay
+//   Future<void> quatro = Future.delayed(Duration(seconds: 3));
+// //aq ele vai imprimir o numero o 4
+//   quatro.then((value) => print(4));
 
   print(1);
   print(2);
   print(3);
-  print(4);
+  quatro();
   print(5);
+}
+
+Future<void> quatro() async {
+  Future.delayed(Duration(seconds: 3), () => print(4));
 }
